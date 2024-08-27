4f610f8908356807ad47816d27ce03a6c9785637
Mark Minehan <minehan.mark@gmail.com>
2024-08-27 16:21:31 +1000

    Set up environment variables

--- api/instagram-token.js
+++ api/instagram-token.js
@@ -1,24 +1,24 @@
+require('dotenv').config();
 const axios = require('axios');
 
-   let instagramToken = process.env.INITIAL_INSTAGRAM_TOKEN;
-   let tokenRefreshDate = new Date();
+let instagramToken = process.env.INITIAL_INSTAGRAM_TOKEN;
+let tokenRefreshDate = new Date();
 
-   async function refreshToken() {
-       try {
-           const response = await axios.get(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${instagramToken}`);
-           instagramToken = response.data.access_token;
-           tokenRefreshDate = new Date();
-           console.log('Token refreshed successfully');
-       } catch (error) {
-           console.error('Error refreshing token:', error);
-       }
-   }
+async function refreshToken() {
+    try {
+        const response = await axios.get(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${instagramToken}`);
+        instagramToken = response.data.access_token;
+        tokenRefreshDate = new Date();
+        console.log('Token refreshed successfully');
+    } catch (error) {
+        console.error('Error refreshing token:', error);
+    }
+}
 
-   module.exports = async (req, res) => {
-       const daysSinceRefresh = (new Date() - tokenRefreshDate) / (1000 * 60 * 60 * 24);
-       if (daysSinceRefresh >= 30) {
-           await refreshToken();
-       }
-       res.json({ token: instagramToken });
-   };
-   
+module.exports = async (req, res) => {
+    const daysSinceRefresh = (new Date() - tokenRefreshDate) / (1000 * 60 * 60 * 24);
+    if (daysSinceRefresh >= 30) {
+        await refreshToken();
+    }
+    res.json({ token: instagramToken });
+};
